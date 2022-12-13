704041cfeb0604f5022e61637815ec9c3c2df361
LuisJoseLopezD <45377927+LuisJoseLopezD@users.noreply.github.com>
2022-12-12 23:34:46 -0400

    first commit

---
+++ server/routes/user.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+
+const mongoose = require('mongoose');
+const products = mongoose.Schema;
+
+const userSchema = new products({
+    id: Number,
+    description: String,
+    imageurl: String
+});
+
+const userModel = mongoose.model('products', userSchema);
+
+//get products
+router.get('/getproducts', async (req, res) => {
+    try{
+        const data = await userModel.find();
+        res.json(data)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+//importar conexion mongodb
+const archivoDB = require('./config/conexion.js');
+
+//import de las rutas y modelo usuario
+const userRoutes = require('./routes/user.js');
+
+// importar body parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:'true'}))
+
+app.use('/api/products', userRoutes);
+
+//rutas
+app.get('/',(req, res) => {
+    res.end('Bievenidos al servidor')
+})
+
+//config server
+app.listen(5000, function(){
+    console.log("el servidor NODE está corriendo");
+})
